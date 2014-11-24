88d439e6b67350a06d159f1b8231c7a2692b0c3d
Dwarfeh <dwarffighter23@gmail.com>
2014-11-24 13:16:22 -0500

    Added project

---
+++ src/DataScraping/Scraper.java
@@ -0,0 +1,23 @@
+package DataScraping;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kevin on 11/15/2014.
+ */
+public class Scraper {
+    private static ArrayList<String> data = new ArrayList<String>();
+
+    public static ArrayList<String> retrieveOverallData(String webURL)
+    {
+        HTMLRetriever retriever = new HTMLRetriever(webURL);
+        retriever.pullData();
+        data = retriever.getPulledData();
+       // retriever.printData();
+        return data;
+
+
+    }
+
+
+}
---
+++ src/DataScraping/WarParser.java
@@ -0,0 +1,8 @@
+package DataScraping;
+
+/**
+ * Created by Kevin on 11/15/2014.
+ */
+public class WarParser {
+
+}
